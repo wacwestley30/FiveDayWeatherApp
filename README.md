87bea07da684039bba9d363e89f36358a36b55a7
wacwestley30 <westleycervantes@gmail.com>
2024-02-06 04:00:03 -0500

    Added functions to better update the history buttons and how they are populated and displayed for the user. Also finished the README except for the link to the live app. One more commit then finished

--- README.md
+++ README.md
@@ -1,37 +1,13 @@
-## User Story
+## Five Day Weather App
 
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
+Link
 
-## Acceptance Criteria
+## Description
 
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
+This is my Five Day Weather App that uses OpenWeatherMap API, DayJS, Jquery, Bootstrap and FontAwesome. This project taught me how to dynamically pull from an API while also furthering my knowledge of localStorage. The OpenWeatherMap API had two collections that I had access to called Current Weather Data and 5 Day Forecast that helped me create this simple weather app.
 
-## Now in English
+## Usage
 
-Multi-city weather app
-////////////////////////
-Dashboard with FORM inputs
+The first time this app is loaded placeholder text will be shown because I did not want to try to access the users current location with this app. There is an input box to the top left of the app that lets the user search for cities. As long as the city is recognized by the API the most relevant (according to the API) city will display along with the city's 5 day forecast. The city will be added to a recent history list of buttons for the user to revisit cities they have searched. A clear history button is also added to clear the users localStorage, the array and the populated buttons. Icons are selected based on the "main" description from the API. I also added the current date to the page just for convenience.
 
-When SEARCHING for a city user is presented with CURRENT and FUTURE conditions
-That city is then added to a SEARCH HISTORY
-
-When viewing a city... User should see:
-City Name, the current date, an ICON representing the weather conditions,
-temperature, humidity, and wind speed.
-
-When viewing FUTURE weather conditions:
-5 day forcast, next 5 days date, ICON representing weather conditions,
-temperature, humidity, and wind speed.
-
-CLICKING on a city in the SEARCH HISTORY should present current
-and future weather conditions.
+![alt text](./assets/Screenshot%202024-02-06%20034241.png)
--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,6 @@
 $(function() {
 
+    // Odds and ends
     let city;
     let cityHistoryArray = [];
     var APIKey = '7b10ccf01a270c6fd2ca6ee3d746b513';
@@ -20,6 +21,7 @@ $(function() {
         cityInput = $('#cityInput'),
         searchForm = $('#searchForm'),
         currentCity = $('#currentCity'),
+        currentDate = $('#currentDate'),
         currentCityTemp = $('#currentCityTemp'),
         currentCityWind = $('#currentCityWind'),
         fiveDayOne = $('#fiveDayOne'),
@@ -55,6 +57,8 @@ $(function() {
         event.stopPropagation();
 
         city = cityInput.val();
+
+        // This handles all the checks I need like if theres even a city in the input and to also make sure only new cities are added to the history buttons and finally to restrict the buttons to 8 recent searches.
         if (!city) {
             alert('Please enter a city');
         } else if (cityHistoryArray.includes(city)) {
@@ -73,6 +77,15 @@ $(function() {
         }
     };
 
+    // Really surprised how easy this was to update the history buttons so that even when you click or search for a city that was already in the list is updated and brought to the top of the list. That bug mentioned later about an extra button still pops up but the reload fixes it.
+    function updateHistoryArray(data) {
+        cityHistoryArray = JSON.parse(localStorage.getItem('cityHistory'));
+        cityHistoryArray.unshift(cityHistoryArray.splice(cityHistoryArray.findIndex(city => city === data.name), 1)[0]);
+        localStorage.setItem('cityHistory', JSON.stringify(cityHistoryArray));
+        displaySearchHistory();
+    }
+
+    // Resets everything back to a workable state no need to reload the page or anything.
     function clearHistory() {
         cityHistoryArray = [];
         localStorage.clear();
@@ -88,14 +101,23 @@ $(function() {
 
     function getCurrentWeather(city) {
         var requestCurrentUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&lang=en&units=imperial&limit=1&appid=' + APIKey;
-        // console.log(city)
         fetch(requestCurrentUrl)
             .then(function (response) {
                 if(response.ok) {
                     response.json().then(function(data) {
                         displayCurrentWeather(data, city);
+                        updateHistoryArray(data);
+                        console.log('current');
+                        console.log(data);
                     })
                 } else {
+                    // This whole respone != ok was 1 line before the very end and doing bug tests. The code works as long as an actual endpoint is reached the other 4 lines were added to catch if a user puts in a bad city and to also repopulate the history buttons
+                    cityHistoryArray.shift();
+                    localStorage.setItem('cityHistory', JSON.stringify(cityHistoryArray));
+
+                    // Very odd bug where an extra button will spawn for the last city in cityHistoryArray so this is the only time the page is reloaded (NEXT LINE) because I don't really know why it is spawning because the Array and localStorage is being updated before the new check for buttons to populate but the extra button is still populating and only the last city no other cities.
+                    location.reload();
+                    displaySearchHistory();
                     alert('Error: ' + response.statusText);
                 }
             })
@@ -104,6 +126,7 @@ $(function() {
             })
     }
 
+    // There is probably a way to iterate but I get confused on how to also iterate through the divs. This happens a few more times in the code so I hard coded just to get it working.
     function displaySearchHistory() {
         if(cityHistoryArray[0] != undefined){
             cityOne.empty();
@@ -137,6 +160,8 @@ $(function() {
             cityEight.empty();
             cityEight.prepend('<button class="btn btn-secondary mb-2">' + cityHistoryArray[7] + '</button>');
         }
+
+        // Don't really know why this has to be here. Normally I put all handlers at the end together but when other displaySearchHistory() is populated the buttons don't work except for the first time they are populated even though this should pull all .btn-secondary regardless.
         $('.btn-secondary').on('click', historyBtnClickEvent);
     }
 
@@ -146,15 +171,18 @@ $(function() {
             return;
         }
 
-        var currentTemperature = Math.round(data.main.temp);
-        var roundedWindSpeed = data.wind.speed.toFixed(1);
+        let currentTemperature = Math.round(data.main.temp);
+        let roundedWindSpeed = data.wind.speed.toFixed(1);
+        let currentDateZero = dayjs().format('dddd MMMM D, YYYY');
 
         currentCity.text(data.name + ', ' + data.sys.country);
+        currentDate.text(currentDateZero);
         currentCityTemp.text(currentTemperature);
         currentCityWind.text(roundedWindSpeed);
         currentCityHumidity.text(data.main.humidity);
     }
 
+    // Really like seperating the get functions from the display functions like some of the examples we have been given up to this point so I used that idea to break up tasks for my project
     function getFiveDayForecast(city) {
         var requestFiveDayUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&lang=en&units=imperial&limit=1&appid=' + APIKey;
 
@@ -162,12 +190,15 @@ $(function() {
             .then(function (response) {
                 if(response.ok) {
                     response.json().then(function(data) {
-                        displayFiveDayForecast(data)
+                        displayFiveDayForecast(data);
+                        console.log('5day');
+                        console.log(data);
                     })
                 }
             })
     }
 
+    // Huge limitation that I haven't got to fully test to fully check but this is a limitation from using the free OpenWeatherMap API. I only get access to 5 day in 3h increment. The problem is if I use a low list number like list[0-6] the API could display the same day depending on when you load the page. 3x7 = 21. So based on that I felt comforable enough starting from list[7] because it would be 21 hours forward so it could still be messed up till 3am but that is also a good app update time. The other issue is it also goes off when the user loads the page. So currently it is 3AM and current list[7] is for 6AM. The problem is that it displays the temp data for 6am and not the average or max for the day. The time of list[7] changes. Could not find anything in the API to specifically target say 3PM there is a 3PM block but the data received changes when the page is loaded. There is a Daily Forecast 16 days but that is only for PAID plans. I left the console.log for both data being pulled to explain what I mean.
     function displayFiveDayForecast(data) {
 
         let dateAndTimeOne = data.list[7].dt_txt;
@@ -234,6 +265,7 @@ $(function() {
         fiveDayHumidityFive.text(data.list[39].main.humidity)
     }
 
+    // Found a list of Common main labels in the API Docs the user could receive and added a switch case for each. I have a console.log in place in case one pops up that I did not account for.
     function iconSelector(i) {
         switch (i) {
             case "Mist":
@@ -256,6 +288,7 @@ $(function() {
         }
     }
 
+    // This checks when user reloads the page or comes back to it if they have localStorage already from the last time they accessed the page.
     if (cityHistoryArray.length == 0 && localStorage.getItem('cityHistory') != null) {
         let previousHistoryArray = JSON.parse(localStorage.getItem('cityHistory'))
         cityHistoryArray = previousHistoryArray;
@@ -265,6 +298,7 @@ $(function() {
         getFiveDayForecast(city);
     };
 
+    // Updates the page when a history button is clicked so that a new button isn't added or localStorage being updated for no reason.
     function historyBtnClickEvent(event) {
         event.preventDefault();
         event.stopPropagation();
